fc0abcb8f77cc93a5c8b1f9c10118999defdb15d
Remy MANSOUR <remy.mansour@outlook.com>
2025-11-16 21:46:41 +0100

    styling

--- src/pages/Champions/ChampionAbilities.tsx
+++ src/pages/Champions/ChampionAbilities.tsx
@@ -8,14 +8,14 @@ export const ChampionAbilities = ({ champ }: { champ: ddChampion }) => {
 
   return (
     <Suspense>
-      <div tw="grid grid-cols-4 gap-2 place-content-center">
+      <div tw="grid grid-cols-4 gap-2 place-content-center text-sm">
         {query.data.data[champ.id].spells.map((s) => (
           <div key={s.id + champ.id} tw="grid [grid-template-columns: auto 1fr] gap-2 place-items-center m-auto">
-            <img src={CHAMPION_IMG_URL(s.image.full, champ.version)} tw="rounded-md" height={40} width={40} />
+            <img src={CHAMPION_IMG_URL(s.image.full, champ.version)} tw="rounded-md" height={45} width={45} />
             <div tw="grid items-center">
               <div tw="flex gap-2 items-baseline">
-                <label tw="font-bold">{s.id.substring(s.id.length - 1)}</label>
-                <label tw="text-sm">{s.name}</label>
+                <label tw="font-bold text-base">{s.id.substring(s.id.length - 1)}</label>
+                <label tw="text-xs">{s.name}</label>
               </div>
               <label>Cooldown(s): {s.cooldownBurn}</label>
               <label>Cost: {s.costBurn}</label>
