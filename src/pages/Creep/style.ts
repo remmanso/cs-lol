f69063f7a6e498efe9e8a8dd38dc06f30b188dfe
Remy MANSOUR <remy.mansour@outlook.com>
2025-11-16 16:19:54 +0100

    styling done

--- src/pages/Creep/style.ts
+++ src/pages/Creep/style.ts
@@ -1,15 +1,15 @@
 import tw, { styled } from "twin.macro";
 
-export const ContainerGrid = tw.div`m-auto grid max-w-5xl select-none gap-2 p-2 md:text-2xl text-xl`;
-export const Container = tw.div`m-auto grid [grid-template-rows: auto 1fr] md:[grid-template-columns: repeat(3, auto)] w-full md:justify-items-center gap-2 md:text-xl text-base font-semibold `;
+export const ContainerGrid = tw.div`m-auto grid max-w-5xl select-none gap-2 p-4 md:text-2xl text-xl`;
+export const Container = tw.div`m-auto grid [grid-template-rows: auto 1fr] md:[grid-template-columns: repeat(3, auto)] w-full md:justify-items-center gap-x-6 gap-y-2 md:text-xl text-base font-semibold `;
 
-export const MinionTile = tw.div`grid grid-cols-2 row-span-1`;
+export const MinionTile = tw.div`grid [grid-template-columns: auto 1fr] row-span-1 w-full`;
 export const GameDuration = tw.div``;
 
 export const MinionGoldCount = tw.div`ml-2 flex gap-1 justify-self-end text-lol-gold`;
 
 export const GoldIcon = styled.img.attrs({ src: "gold-icon.png", title: "gold" })(() => [
-  tw`[height: 11px] [width: 11px] self-end mb-1`,
+  tw`[height: 11px] [width: 11px] md:[height: 13px] md:[width: 13px] self-end mb-1`,
 ]);
 
 export const IncomeBreakDown = {
@@ -17,6 +17,6 @@ export const IncomeBreakDown = {
   Title: tw.h2`pb-1 md:text-xl`,
   Value: tw.div`inline-flex gap-1 text-lol-gold justify-self-end ml-4`,
   ListContainer: tw.span`grid [grid-template-columns: auto 1fr]`,
-  ListItem: tw.li`ml-10 [list-style-type: "- "] [text-indent: 5px]`,
+  ListItem: tw.li`pl-2 md:pl-4 border-b-2 border-white/10 pb-0.5 `,
   NoListItem: tw.li`list-none w-full`,
 };
--- src/styles/style.ts
+++ src/styles/style.ts
@@ -1,3 +1,3 @@
 import tw from "twin.macro";
 
-export const Container = tw.div`h-screen w-screen overflow-auto bg-lol-client-bg font-medium text-white`;
+export const Container = tw.div`h-screen w-screen overflow-auto bg-lol-client-bg font-medium text-white p-0 md:p-4 `;
