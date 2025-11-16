fc199f2b05d3c317cb3f11259157780c0d902099
Remy MANSOUR <remy.mansour@outlook.com>
2025-11-16 21:39:41 +0100

    adding styling abilities

--- src/pages/Champions/ChampionAbilities.tsx
+++ src/pages/Champions/ChampionAbilities.tsx
@@ -12,11 +12,14 @@ export const ChampionAbilities = ({ champ }: { champ: ddChampion }) => {
         {query.data.data[champ.id].spells.map((s) => (
           <div key={s.id + champ.id} tw="grid [grid-template-columns: auto 1fr] gap-2 place-items-center m-auto">
             <img src={CHAMPION_IMG_URL(s.image.full, champ.version)} tw="rounded-md" height={40} width={40} />
-            <div tw="grid grid-rows-2 items-center">
-              <div tw="">
-                {s.id.substring(s.id.length - 1)} <label tw="text-sm">{s.name}</label>
+            <div tw="grid items-center">
+              <div tw="flex gap-2 items-baseline">
+                <label tw="font-bold">{s.id.substring(s.id.length - 1)}</label>
+                <label tw="text-sm">{s.name}</label>
               </div>
-              <label>{s.cooldownBurn}</label>
+              <label>Cooldown(s): {s.cooldownBurn}</label>
+              <label>Cost: {s.costBurn}</label>
+              {/* <p>{s.description}</p> */}
             </div>
           </div>
         ))}
