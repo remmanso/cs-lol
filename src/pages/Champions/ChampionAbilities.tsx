5b97c5b265cffc06e8434657fc7f608aa4b9f897
Remy MANSOUR <remy.mansour@outlook.com>
2025-11-17 13:28:47 +0100

    changing template

--- src/pages/Champions/ChampionAbilities.tsx
+++ src/pages/Champions/ChampionAbilities.tsx
@@ -10,7 +10,7 @@ export const ChampionAbilities = ({ champ }: { champ: ddChampion }) => {
 
   return (
     <Suspense>
-      <div tw="grid xs:[grid-template-columns: repeat(auto-fit, minmax(25%, 1fr) minmax(25%, 1fr))] sm:grid-cols-4 gap-8 place-content-center text-sm">
+      <div tw="grid xs:[grid-template-columns: repeat(auto-fit, minmax(140px, 0.5fr) minmax(140px, 0.5fr))] md:grid-cols-4 lg:gap-8 gap-4 place-content-center text-sm">
         {query.data.data[champ.id].spells.map((s, i) => (
           <div
             key={s.id + champ.id}
@@ -23,13 +23,13 @@ export const ChampionAbilities = ({ champ }: { champ: ddChampion }) => {
               width={45}
               title={s.description}
             />
-            <div tw="grid [grid-template-columns: repeat(auto-fit, auto)]">
-              <div tw="grid gap-2 items-center [grid-template-columns: auto 1fr] place-self-stretch justify-items-stretch">
+            <div tw="grid [grid-template-columns: repeat(auto-fit, auto)] [grid-template-rows: 32px]">
+              <div tw="grid lg:gap-2 gap-1 items-center [grid-template-columns: auto 1fr] place-self-stretch justify-items-stretch">
                 <label tw="font-bold text-base">{i < spellsName.length ? spellsName[i] : "more spells ?"}</label>
                 <label tw="text-xs [text-overflow: clip] text-end [text-size-adjust: ]">{s.name}</label>
               </div>
 
-              <label tw="grid [grid-template-columns: auto 1fr] md:gap-1">
+              <label tw="grid [grid-template-columns: 5ch 1fr] md:gap-1">
                 <b tw="text-xs">Cd(s):</b>
                 <div tw="grid gap-0.5 [grid-template-columns: repeat(auto-fit, minmax(2ch, 3ch))] lg:gap-2 text-xs text-end justify-end">
                   {s.cooldown.map((c, i) => (
@@ -37,7 +37,7 @@ export const ChampionAbilities = ({ champ }: { champ: ddChampion }) => {
                   ))}
                 </div>
               </label>
-              <label tw="grid [grid-template-columns: auto 1fr] justify-items-stretch">
+              <label tw="grid [grid-template-columns: 5ch 1fr] justify-items-stretch">
                 <b tw="text-xs">Cost:</b>
                 <div tw="grid gap-0.5 [grid-template-columns: repeat(auto-fit, minmax(2ch, 3ch))] lg:gap-2 text-xs text-end justify-end">
                   {s.cost.map((c, i) => (
